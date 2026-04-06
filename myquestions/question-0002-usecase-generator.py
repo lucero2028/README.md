6c5d3f8aa2d2819416466cd6048f06faffde3ed1
lucero2028 <lucero.florez@udea.edu.co>
2026-04-06 17:35:54 -0500

    Create question-0002-usecase-generator.py

---
+++ myquestions/question-0002-usecase-generator.py
@@ -0,0 +1,8 @@
+
+i, o = generar_caso_de_uso_comparar_modelos_clasificacion()
+
+print('---- inputs ----')
+for k, v in i.items():
+    print("\n", k, ":\n", v)
+
+print('\n---- expected output ----\n', o)
